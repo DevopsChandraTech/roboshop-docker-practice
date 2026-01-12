39b1a852841c34b353583976c3490fb47ba3aa5b
DevOpsChandraTech <devopschandra42@gmail.com>
2026-01-12 19:15:57 +0530

    roboshop-docker

--- mysql/db/schema.sql
+++ mysql/db/schema.sql
@@ -4,15 +4,15 @@ use cities;
 
 DROP TABLE IF EXISTS `cities`;
 CREATE TABLE `cities` (
-    `uuid` int(11) NOT NULL AUTO_INCREMENT,
-    `country_code` varchar(2) DEFAULT NULL,
-    `city` varchar(100) DEFAULT NULL,
-    `name` varchar(100) DEFAULT NULL,
-    `region` varchar(100) DEFAULT NULL,
-    `latitude` decimal(10,7) DEFAULT NULL,
-    `longitude` decimal(10,7) DEFAULT NULL,
-    PRIMARY KEY (`uudi`),
-    KEY `region_idx` (`region`),
-    KEY `c_code_idx` (`country_code`),
-    FULLTEXT KEY `city_idx` (`city`)
+  `uuid` int(11) NOT NULL AUTO_INCREMENT,
+  `country_code` varchar(2) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `region` varchar(100) DEFAULT NULL,
+  `latitude` decimal(10,7) DEFAULT NULL,
+  `longitude` decimal(10,7) DEFAULT NULL,
+  PRIMARY KEY (`uudi`),
+  KEY `region_idx` (`region`),
+  KEY `c_code_idx` (`country_code`),
+  FULLTEXT KEY `city_idx` (`city`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6223666 DEFAULT CHARSET=latin1;
